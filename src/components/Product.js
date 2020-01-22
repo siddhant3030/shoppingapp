d023fc6c8bfd50c1ecb095a3b9cff7e872a951eb
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-22 17:19:46 +0530

    css for card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import {ProductConsumer} from '../context';
+import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
     render() {
@@ -9,10 +9,23 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log("clicked")}>
+                    <div className="img-container p-5" onClick={() => console.log("clicked")}>
                         <Link to='/details'>
                             <img src={img} alt= "product" className="card-img-top"/>
                         </Link>
+                        <button className="card-btn" disabled={inCart?true:false} onClick={()=>{console.log('added to cart')}}>
+                        {inCart?(<p className="text-capitalize mb-0" disabled>in Cart</p>):(<i className="fas fa-cart-plus"/>)}
+                        </button>
+                    </div>
+                    {/* card footer */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
@@ -21,5 +34,8 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div `
-
+.card {
+    border-color: transparent;
+    transition: all ls linear;
+}
 `
